3eb43dbd0af4f16d1c74616530a7bc6f84e7aa89
cel3ntano <adriano2024celentano@gmail.com>
2025-03-27 22:55:52 +0100

    chore: add react-hook-form and zod, update dependencies
    - Integrate react-hook-form with zod validation in LoginForm
    - Update project dependencies

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,10 +1,25 @@
+'use client';
+
 import { Card, CardBody, CardHeader } from '@heroui/card';
 import { Input } from '@heroui/input';
 import { Button } from '@heroui/button';
 import React from 'react';
 import { GiPadlock } from 'react-icons/gi';
+import { useForm } from 'react-hook-form';
+import { loginSchema, LoginSchema } from '@/lib/schemas/loginSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onTouched',
+  });
+  const onSubmit = (data: LoginSchema) => console.log(data);
+
   return (
     <Card className="w-2/5 mx-auto">
       <CardHeader className="flex flex-col items-center justify-center">
@@ -17,11 +32,31 @@ export default function LoginForm() {
         </div>
       </CardHeader>
       <CardBody>
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-4">
-            <Input label="Email" variant="bordered" />
-            <Input label="Password" variant="bordered" type="pasword" />
-            <Button fullWidth color="secondary" type="submit">
+            <Input
+              defaultValue=""
+              label="Email"
+              variant="bordered"
+              {...register('email')}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message as string}
+            />
+            <Input
+              defaultValue=""
+              label="Password"
+              variant="bordered"
+              type="pasword"
+              {...register('password')}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message as string}
+            />
+            <Button
+              isDisabled={!isValid}
+              fullWidth
+              color="secondary"
+              type="submit"
+            >
               Login
             </Button>
           </div>
