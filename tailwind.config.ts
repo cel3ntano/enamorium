6c651633d9276f2207e61176b2f348fd8710a687
cel3ntano <adriano2024celentano@gmail.com>
2025-04-20 22:48:13 +0200

    style(MemberCard): add styles to enhance component appearance

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,21 @@
-import type { Config } from "tailwindcss";
-import { heroui } from "@heroui/react";
+import type { Config } from 'tailwindcss';
+import { heroui } from '@heroui/react';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: {
+        'dark-gradient':
+          'linear-gradient(to top, rgba(0,0,0,0.8), transparent)',
+      },
+    },
+  },
   darkMode: 'class',
   plugins: [heroui()],
 };
