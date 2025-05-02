336bf1165dfa0a6a123b1c3cb35db0e0232b6f00
Andrii Zhygalko <andrii.zhygalko@gmail.com>
2025-05-02 23:46:28 +0200

    feat(like): add fetching logic for likes

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { getMembers } from '../actions/memberActions';
 import MemberCard from './MemberCard';
+import { fetchCurrntUserLikeIds } from '../actions/likeActions';
 
 export default async function MembersPage() {
   const members = await getMembers();
+  const likeIds = await fetchCurrntUserLikeIds();
 
   return (
     <div className='mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-8'>
       {members &&
-        members.map((member) => <MemberCard key={member.id} member={member} />)}
+        members.map((member) => (
+          <MemberCard key={member.id} member={member} likeIds={likeIds} />
+        ))}
     </div>
   );
 }
